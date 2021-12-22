ba9894e7a911244941182d5ec55ae01d83051e3f
cozy <meaningful.jy@gmail.com>
2021-12-22 17:14:55 +0900

    loading weather, It works!!!
    bug fixed.
    
    fix by,
    original url + "&units=matric"

--- weather.js
+++ weather.js
@@ -10,30 +10,40 @@ function onGeoSuccess(position) {
     
     const serviceKey = 'bd8617a96c9fb01ca5def927dbd4a161';
 
-    const air_pollutionUrl= `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${serviceKey}`;
-    const curWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${serviceKey}`;
+    const air_pollutionUrl= `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${serviceKey}&units=matric`;
+    const curWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${serviceKey}&units=matric`;
     
-    const pm_2_5 = getDataWeatherAPIcall(curWeatherUrl)
-    airSpan.innerText= pm_2_5.list[0].components.pm2_5;
-    // 75 > 매우 나쁨   / 좋음
-    const weatherInfo = getDataWeatherAPIcall(air_pollutionUrl)
-    citySpan.innerText = weatherInfo.name;
-    tempSpan.innerText = weatherInfo.weather[0].main.temp;
+    getDataWeatherAPIcall(curWeatherUrl,air_pollutionUrl)
 
-    geolocaDiv.appendChild(airSpan);
-    geolocaDiv.appendChild(tempSpan);
-    geolocaDiv.appendChild(citySpan);
+
+
 
 }
 function onGeoError(){
     alert("Opps, we cann't get your POS.")
 }
 
-function getDataWeatherAPIcall(url){
+function getDataWeatherAPIcall(curWeatherUrl, air_pollutionUrl){
     
-    console.log(url);
-    fetch(url).then(res=>res.json())
-    .then(data => {console.log(data.name);
+    console.log(curWeatherUrl);
+    console.log(air_pollutionUrl);
+
+    fetch(curWeatherUrl).then(res=>res.json())
+    .then(weatherInfo => {
+        console.log(weatherInfo.name,weatherInfo.weather[0].main);
+        citySpan.innerText = weatherInfo.name + ' ';
+        tempSpan.innerText = weatherInfo.weather[0].main+ ' ';
+
+        geolocaDiv.appendChild(tempSpan);
+        geolocaDiv.appendChild(citySpan);
+    }).catch(e=>console.log(e));
+    
+    fetch(air_pollutionUrl).then(res=>res.json())
+    .then(airInfo => {
+        console.log(airInfo.list[0].components.pm2_5);
+        airSpan.innerText= '미세먼지 : '+ airInfo.list[0].components.pm2_5 + ' ';
+        // 75 > 매우 나쁨   / 좋음 
+        geolocaDiv.appendChild(airSpan);
     }).catch(e=>console.log(e));
 }
 
